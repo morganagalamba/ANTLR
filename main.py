e4336b73400d00c92b709ff37d8025cf8766154c
Morgana Galamba <mbfg2@cin.ufpe.br>
2023-08-15 22:18:30 -0300

    modifica func e roda em loop

--- main.py
+++ main.py
@@ -3,15 +3,45 @@ from ArithmeticLexer import ArithmeticLexer
 from ArithmeticParser import ArithmeticParser
 
 class ArithmeticVisitor:
+    def __init__(self):
+        self.variables = {}
+
     def visit(self, ctx):
-        if isinstance(ctx, ArithmeticParser.ExprContext):
+        if isinstance(ctx, ArithmeticParser.ProgramContext):
+            return self.visitProgram(ctx)
+        elif isinstance(ctx, ArithmeticParser.StatementContext):
+            return self.visitStatement(ctx)
+        elif isinstance(ctx, ArithmeticParser.AssignmentContext):
+            return self.visitAssignment(ctx)
+        elif isinstance(ctx, ArithmeticParser.ExprContext):
             return self.visitExpr(ctx)
         elif isinstance(ctx, ArithmeticParser.TermContext):
             return self.visitTerm(ctx)
         elif isinstance(ctx, ArithmeticParser.FactorContext):
             return self.visitFactor(ctx)
+    
+    def visitProgram(self, ctx):
+        result = None
+        for statement in ctx.statement():
+            result = self.visit(statement)
+        return result
+    
+    def visitStatement(self, ctx):
+        if ctx.assignment():
+            return self.visit(ctx.assignment())
+        else:
+            return self.visit(ctx.expr())
+
+    def visitAssignment(self, ctx):
+        var_name = ctx.VAR().getText()
+        var_value = self.visit(ctx.expr())
+        self.variables[var_name] = var_value
+        return var_value
 
     def visitExpr(self, ctx):
+        if len(ctx.term()) == 1:
+            return self.visit(ctx.term(0))
+        
         result = self.visit(ctx.term(0))
         for i in range(1, len(ctx.term())):
             if ctx.getChild(i * 2 - 1).getText() == '+':
@@ -32,18 +62,31 @@ class ArithmeticVisitor:
     def visitFactor(self, ctx):
         if ctx.INT():
             return int(ctx.INT().getText())
+        elif ctx.VAR():
+            var_name = ctx.VAR().getText()
+            if var_name in self.variables:
+                return self.variables[var_name]
+            else:
+                raise ValueError(f"Variable '{var_name}' is not defined.")
         else:
             return self.visit(ctx.expr())
 
 def main():
-    expression = input("Digite uma expressão aritmética: ")
-    lexer = ArithmeticLexer(InputStream(expression))
-    stream = CommonTokenStream(lexer)
-    parser = ArithmeticParser(stream)
-    tree = parser.expr()
     visitor = ArithmeticVisitor()
-    result = visitor.visit(tree)
-    print("Resultado:", result)
+    while True:
+        expression = input("Digite uma instrução ou expressão: ")
+        if expression == "exit":
+            break
+        lexer = ArithmeticLexer(InputStream(expression))
+        stream = CommonTokenStream(lexer)
+        parser = ArithmeticParser(stream)
+        tree = parser.program()
+        try:
+            result = visitor.visit(tree)
+            if result is not None:
+                print("Resultado:", result)
+        except Exception as e:
+            print("Erro:", e)
 
 if __name__ == '__main__':
     main()
